8ce9f57f5e9d2e1640a86ca8149552fdfd9b828b
Henrique Geenen <henriqueggo@hotmail.com>
2025-02-27 01:15:57 -0300

    fix application load contexts test

---
+++ src/test/java/br/com/hackaton/HackatonApplicationTests.java
@@ -0,0 +1,15 @@
+package br.com.hackaton;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest
+@ActiveProfiles("test")
+class HackatonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
