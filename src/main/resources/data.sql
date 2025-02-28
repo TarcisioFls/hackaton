64a3d76a230a4ca56a994e5782ed901dd23cdd84
Henrique Geenen <henriqueggo@hotmail.com>
2025-02-27 22:07:09 -0300

    changes on data sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,7 +1,9 @@
 -- Create Endereco
 INSERT INTO endereco (id, cep, logradouro, numero, complemento, bairro, cidade, estado, latitude, longitude) VALUES
 (1, '01001-000', 'Rua das Flores', '123', 'Apt 101', 'Centro', 'São Paulo', 'SP', -23.550520, -46.633308),
-(2, '02002-000', 'Avenida Paulista', '456', 'Suite 202', 'Bela Vista', 'São Paulo', 'SP', -23.561414, -46.655881);
+(2, '02002-000', 'Avenida Paulista', '456', 'Suite 202', 'Bela Vista', 'São Paulo', 'SP', -23.561414, -46.655881),
+(3, '03003-000', 'Rua Augusta', '789', 'Casa 303', 'Consolação', 'São Paulo', 'SP', -23.548943, -46.638818),
+(4, '04004-000', 'Rua Oscar Freire', '1011', 'Sala 404', 'Jardins', 'São Paulo', 'SP', -23.566347, -46.672527);
 
 -- Create UBS
 INSERT INTO ubs (id, nome, telefone, inicio_atendimento, fim_atendimento, endereco_id) VALUES
@@ -27,8 +29,8 @@ INSERT INTO medico_especialidades (medico_id, especialidades) VALUES
 
 -- Create Paciente
 INSERT INTO paciente (id, nome, email, cpf, telefone, cns, endereco_id) VALUES
-(1, 'Joao Souza', 'joao.souza@email.com', '123.456.789-00', '987-654-3210', '123456789012345', 2),
-(2, 'Maria Braga', 'maria.braga@email.com', '987.654.321-00', '654-321-0987', '987654321098765', 1);
+(1, 'Joao Souza', 'joao.souza@email.com', '123.456.789-00', '987-654-3210', '123456789012345', 3),
+(2, 'Maria Braga', 'maria.braga@email.com', '987.654.321-00', '654-321-0987', '987654321098765', 4);
 
 -- Create Receita
 INSERT INTO receita (id, medico_id, paciente_id, data_hora_criacao) VALUES
